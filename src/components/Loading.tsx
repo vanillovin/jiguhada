0ecfba3872817c325483462f31be02bad65c5acd
vanillovin <vanillovin@gmail.com>
2022-09-20 15:56:03 +0900

    add: Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+function Loading() {
+  return (
+    <div className="loading">
+      <span>L</span>
+      <span>O</span>
+      <span>A</span>
+      <span>D</span>
+      <span>I</span>
+      <span>N</span>
+      <span>G</span>
+    </div>
+  );
+}
+
+export default Loading;
