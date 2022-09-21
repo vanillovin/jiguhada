f28cfd24e12a29b3783cd685168cc8a6343d45a1
vanillovin <vanillovin@gmail.com>
2022-09-21 21:44:06 +0900

    import React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import './index.css';
-// tailwind base style? 에 먹혀서..? 옮김
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-// Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* Provide the client to your App */}
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <App />
       </RecoilRoot>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root') as HTMLElement
