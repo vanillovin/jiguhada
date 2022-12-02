fd8c19b3b5b2a93dcef33db2894c633621386760
vanillovin <vanillovin@gmail.com>
2022-12-02 17:54:26 +0900

    QueryClient의 Default Options 적용

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,16 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: Infinity,
+      refetchOnWindowFocus: false, // window 가 다시 포커스 될 때
+      // refetchOnMount // 쿼리의 새 인스턴스가 마운트 될 때
+      // refetchOnReconnect // 네트워크가 끊어졌다가 다시 연결될 때
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
