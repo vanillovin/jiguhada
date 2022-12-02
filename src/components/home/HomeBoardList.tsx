96e9b65160102980b1b72d17c97a35cf0c13e593
vanillovin <vanillovin@gmail.com>
2022-12-02 10:16:54 +0900

    add: HomeBoardList 컴포넌트 분리

---
+++ src/components/home/HomeBoardList.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { getBoardList } from '../../modules/board/api';
+import { Order } from '../../modules/board/type';
+import { displayedAt, getBoardCatText } from '../../utils';
+
+function HomeBoardList({ order }: { order: Order }): JSX.Element {
+  const navigate = useNavigate();
+  const { data } = useQuery(['HomeBoard', order], () => getBoardList({ order }));
+
+  return (
+    <ul className="mt-1">
+      {data?.boardItemList.slice(0, 10).map((pb, i) => (
+        <li
+          key={pb.boardId}
+          className={`cursor-pointer h-8 px-1 ${
+            i === 9 ? 'border-0' : 'border-b'
+          } hover:bg-gray-100  transition-all flex items-center justify-between`}
+          onClick={() => navigate(`board/${pb.boardId}`)}
+        >
+          <h4 className="text-sm md:text-base truncate flex-1">
+            <span className="mr-1 font-medium">[{getBoardCatText(pb.category)}]</span>
+            <span>{pb.boardTitle}</span>
+          </h4>
+          <h5 className="text-2xs md:text-xs text-gray-4 w-14 text-end">
+            <span>{displayedAt(new Date(pb.createDate).getTime())}</span>
+            {/* <span className="font-medium ml-1 text-gray-5">{pb.writer}</span> */}
+          </h5>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default HomeBoardList;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,8 @@
-import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import { getBoardList } from '../modules/board/api';
-import { displayedAt, getBoardCatText, getDateText } from '../utils';
+import HomeBoardList from '../components/home/HomeBoardList';
 
 function Home() {
   const navigate = useNavigate();
-  const { data: recentBoardList } = useQuery([`HomeRecentBoard`], () =>
-    getBoardList({ order: 'RECENT' })
-  );
-  const { data: popularBoardList } = useQuery([`HomePopularBoard`], () =>
-    getBoardList({ order: 'POPULAR' })
-  );
 
   return (
     <main className="w-full max-w-6xl px-5 md:px-10">
@@ -32,58 +24,12 @@ function Home() {
           <div className="grid grid-cols-2 gap-4">
             <div className="">
               <h3 className="text-base md:text-lg font-semibold mt-2">인기 게시글 ✨</h3>
-              <ul className="mt-1">
-                {popularBoardList?.boardItemList.slice(0, 10).map((pb, i) => (
-                  <li
-                    key={pb.boardId}
-                    className={`cursor-pointer p-1 ${
-                      i === 9 ? 'border-0' : 'border-b'
-                    } hover:bg-gray-1`}
-                    onClick={() => navigate(`board/${pb.boardId}`)}
-                  >
-                    <div className="flex items-center justify-between">
-                      <h4 className="text-sm md:text-base">
-                        <span className="mr-1 font-medium">
-                          [{getBoardCatText(pb.category)}]
-                        </span>
-                        {pb.boardTitle}
-                      </h4>
-                      <h5 className="text-2xs md:text-xs text-gray-4 ml-2">
-                        <span>{displayedAt(new Date(pb.createDate).getTime())}</span>
-                        {/* <span className="font-medium ml-1 text-gray-5">{pb.writer}</span> */}
-                      </h5>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              <HomeBoardList order="POPULAR" />
             </div>
 
             <div className="">
               <h3 className="text-base md:text-lg font-semibold mt-2">최신 게시글 🔥</h3>
-              <ul className="mt-1">
-                {recentBoardList?.boardItemList.slice(0, 10).map((pb, i) => (
-                  <li
-                    key={pb.boardId}
-                    className={`cursor-pointer p-1 ${
-                      i === 9 ? 'border-0' : 'border-b'
-                    } hover:bg-gray-1`}
-                    onClick={() => navigate(`board/${pb.boardId}`)}
-                  >
-                    <div className="flex items-center justify-between">
-                      <h4 className="text-sm md:text-base">
-                        <span className="mr-1 font-medium">
-                          [{getBoardCatText(pb.category)}]
-                        </span>
-                        {pb.boardTitle}
-                      </h4>
-                      <h5 className="text-2xs md:text-xs text-gray-4 ml-2">
-                        <span>{displayedAt(new Date(pb.createDate).getTime())}</span>
-                        {/* <span className="font-medium ml-1 text-gray-5">{pb.writer}</span> */}
-                      </h5>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              <HomeBoardList order="RECENT" />
             </div>
           </div>
         </article>
